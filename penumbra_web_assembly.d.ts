88affe3aad075029609db5a542dcb8161a5b9017
Valentine <valentine@zpoken.io>
2022-09-15 03:43:33 +0300

    029-eukelade

---
+++ penumbra_web_assembly.d.ts
@@ -0,0 +1,9 @@
+/* tslint:disable */
+/* eslint-disable */
+/**
+* @param {string} full_viewing_key
+* @param {string} encrypted_note
+* @param {string} ephemeral_key
+* @returns {any}
+*/
+export function decrypt_note(full_viewing_key: string, encrypted_note: string, ephemeral_key: string): any;
---
+++ penumbra_web_assembly.js
@@ -0,0 +1,2 @@
+import * as wasm from "./penumbra_web_assembly_bg.wasm";
+export * from "./penumbra_web_assembly_bg.js";
